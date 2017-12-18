b91459447176725fd3252ae54aeaee32d21c80a5
jon-snow <sarna.kunal07@gmail.com>
2017-12-19 02:56:38 +0530

    design changes

--- app/src/main/java/com/android/hospitalapplication/SplashActivity.java
+++ app/src/main/java/com/android/hospitalapplication/SplashActivity.java
@@ -2,7 +2,8 @@ package com.android.hospitalapplication;
 
 import android.content.Intent;
 import android.os.Build;
-import android.support.constraint.ConstraintLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,14 +24,16 @@ public class SplashActivity extends AppCompatActivity {
     FirebaseAuth auth = FirebaseAuth.getInstance();
     DatabaseReference dbrefUser;
 
-    ConstraintLayout constraintLayout;
+    LinearLayout LinearLayout;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_splash);
 
         fadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
-        constraintLayout = (ConstraintLayout) findViewById(R.id.splash_layout);
+        LinearLayout = (LinearLayout) findViewById(R.id.splash_layout);
+
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
@@ -84,7 +87,7 @@ public class SplashActivity extends AppCompatActivity {
             }
         });
 
-        constraintLayout.setAnimation(fadeIn);
+        LinearLayout.setAnimation(fadeIn);
 
 
     }
